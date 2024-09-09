d2c872414c0f514e97ef6179ae713946a9cc294d
Daisuke <mhdf23888@gmail.com>
2024-09-10 07:31:48 +0800

    adding "s01/appy_s01_01_06.py" on 10/Sep/2024

---
+++ s01/appy_s01_01_06.py
@@ -0,0 +1,20 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/09/09 15:10:13 (UT+8) daisuke>
+#
+
+# importing math module
+import math
+
+# a number
+a = 3.0
+
+# calculation of square root
+b = math.sqrt (a)
+c = a**0.5
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = math.sqrt ({a}) = {b}')
+print (f'c = a^0.5 = {a}^0.5 = {c}')
