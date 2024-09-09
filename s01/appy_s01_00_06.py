fd67e5a75708a45f8d92785082c4de3e0a99f6f9
Daisuke <mhdf23888@gmail.com>
2024-09-10 07:31:25 +0800

    adding "s01/appy_s01_00_06.py" on 10/Sep/2024

---
+++ s01/appy_s01_00_06.py
@@ -0,0 +1,13 @@
+
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/09/09 16:00:06 (UT+8) daisuke>
+#
+
+# assignment of a value to a variable
+a = 1.23 * 10**12
+
+# printing the value of a variable using built-in function "print ()"
+# and printf-style string formatting
+print ('a = %g' % a)
