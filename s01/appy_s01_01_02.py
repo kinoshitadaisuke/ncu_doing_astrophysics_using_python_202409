312af0b3826e023972ca088327f4d799658c1d54
Daisuke <mhdf23888@gmail.com>
2024-09-10 07:31:42 +0800

    adding "s01/appy_s01_01_02.py" on 10/Sep/2024

---
+++ s01/appy_s01_01_02.py
@@ -0,0 +1,16 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/09/09 14:51:55 (UT+8) daisuke>
+#
+
+# two numbers
+a, b = 13, 17
+
+# calculation
+c = a * b
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = {b}')
+print (f'c = a * b = {a} * {b} = {c}')
