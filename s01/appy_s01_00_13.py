8de7f7df4baa3c29cae1785a0ac20a007a1150bb
Daisuke <mhdf23888@gmail.com>
2024-09-10 07:31:36 +0800

    adding "s01/appy_s01_00_13.py" on 10/Sep/2024

---
+++ s01/appy_s01_00_13.py
@@ -0,0 +1,15 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/09/09 14:34:44 (UT+8) daisuke>
+#
+
+# assignment of variables
+c  = 299792458
+au = 149597870700
+pc = 3.08567758 * 10**16
+
+# fancy formatting using .format () method
+print ('c    = {:g} [m/s]'.format (c))
+print ('1 au = {:g} [m]'.format (au))
+print ('1 pc = {:g} [m]'.format (pc))
