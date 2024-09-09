db0ff0493eaf2b1ea3ff941a5fc1190b8deb96d1
Daisuke <mhdf23888@gmail.com>
2024-09-10 07:31:22 +0800

    adding "s01/appy_s01_00_04.py" on 10/Sep/2024

---
+++ s01/appy_s01_00_04.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/09/09 11:54:59 (UT+8) daisuke>
+#
+
+# assignment of a value to a variable
+a = 456.789
+
+# printing the value of a variable using built-in function "print ()"
+print ('a =', a)
