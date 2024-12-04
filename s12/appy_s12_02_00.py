84442b3f0ea8a68dc5431f313c93dc0a4ee7ada9
Daisuke <mhdf23888@gmail.com>
2024-12-04 16:07:33 +0800

    adding "s12/appy_s12_02_00.py" on  4/Dec/2024

--- s12/appy_s12_02_00.py
+++ s12/appy_s12_02_00.py
@@ -1,75 +1,47 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/05 16:45:10 (UT+8) daisuke>
+# Time-stamp: <2024/12/04 11:47:15 (UT+8) daisuke>
 #
 
-# importing numpy module
-import numpy
+# importing urllib module
+import urllib.request
 
-# importing matplotlib module
-import matplotlib.figure
-import matplotlib.backends.backend_agg
+# importing ssl module
+import ssl
 
-# input file
-file_input = '201498.tb2.txt'
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# output file
-file_output = 'appy_s12_02_00.png'
+# URL of data file
+url_data = 'https://iopscience.iop.org/article/10.1086/339557/fulltext/201498.tb2.txt?doi=10.1086/339557'
 
-# numpy arrays to store data
-data_jd      = numpy.array ([])
-data_mag_app = numpy.array ([])
-data_mag_abs = numpy.array ([])
+# output file name
+file_output = '201498.tb2.txt'
 
-# opening file
-with open (file_input, 'r') as fh:
-    # reading data line-by-line
-    for line in fh:
-        # skipping line if the line starts with '#'
-        if (line[0] == '#'):
-            continue
-        # skipping line if the line does not start with digits
-        if not (line[0].isdigit):
-            continue
-        # skipping line if it is empty
-        if (line.strip () == ''):
-            continue
-        # removing line feed at the end of line
-        line = line.strip ()
-        # splitting data
-        (frame_id_str, month_str, day_str, jd_str, mag_app_str, mag_abs_str) \
-            = line.split ()
-        # conversion from string into float
-        frame_id = float (frame_id_str)
-        day      = float (day_str)
-        jd_str   = jd_str.replace (',', '')
-        jd       = float (jd_str)
-        mag_app  = float (mag_app_str)
-        mag_abs  = float (mag_abs_str)
+# printing status
+print (f'Now, fetching {url_data}...')
 
-        # appending the data at the end of numpy arrays
-        data_jd      = numpy.append (data_jd, jd)
-        data_mag_app = numpy.append (data_mag_app, mag_app)
-        data_mag_abs = numpy.append (data_mag_abs, mag_abs)
-        
-# making fig and ax
-fig    = matplotlib.figure.Figure ()
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
-ax     = fig.add_subplot (111)
+# making request object
+req = urllib.request.Request (url_data)
+req.add_header ('Referer', 'https://iopscience.iop.org/article/10.1086/339557/fulltext/')
+req.add_header ('User-Agent', 'Mozilla/5.0 (X11; NetBSD x86_64; rv:128.0) Gecko/20100101 Firefox/128.0')
 
-# labels
-ax.set_xlabel ('JD - 2450000')
-ax.set_ylabel ('R-band Absolute Magnitude [mag]')
+# opening URL
+with urllib.request.urlopen (req) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
 
-# axes
-ax.invert_yaxis ()
+# printing status
+print (f'Finished fetching {url_data}!')
 
-# plotting a figure
-ax.plot (data_jd, data_mag_abs, \
-         linestyle='None', marker='o', markersize=3, color='red', \
-         label='(20000) Varuna')
-ax.legend ()
+# printing status
+print (f'Now, writing data into file "{file_output}"...')
 
-# saving the figure to a file
-fig.savefig (file_output, dpi=150)
+# opening file for writing
+with open (file_output, 'wb') as fh_write:
+    # writing data
+    fh_write.write (data_byte)
+
+# printing status
+print (f'Finished writing data into file "{file_output}"!')
