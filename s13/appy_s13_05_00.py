2f6cef2312373c8373a02e8f6ab7df2afc14196b
Daisuke <mhdf23888@gmail.com>
2024-12-11 07:31:06 +0800

    adding "s13/appy_s13_05_00.py" on 11/Dec/2024

--- s13/appy_s13_05_00.py
+++ s13/appy_s13_05_00.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/12 08:40:07 (UT+8) daisuke>
+# Time-stamp: <2024/12/11 07:14:08 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -113,12 +113,12 @@ elif (name_resolver == 'ned'):
     query_result = astroquery.ipac.ned.Ned.query_object (target_name)
 
 # RA and Dec
-RA  = query_result['RA']
-Dec = query_result['DEC']
+RA  = query_result['ra']
+Dec = query_result['dec']
 
 # coordinate
 if (name_resolver == 'simbad'):
-    coord = astropy.coordinates.SkyCoord (RA[0], Dec[0], unit=(u_ha, u_deg))
+    coord = astropy.coordinates.SkyCoord (RA[0], Dec[0], unit=(u_deg, u_deg))
 elif (name_resolver == 'ned'):
     coord = astropy.coordinates.SkyCoord (RA[0], Dec[0], unit=(u_deg, u_deg))
 
